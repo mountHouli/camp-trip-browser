2d01154c45f2ae11653a18f09be937ed1978fe51
Aaron Houlihan <AaronRobertHoulihan@gmail.com>
2018-03-06 20:04:48 -0500

    bug fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import SomeComponent from './components/components/SomeComponent'
+import SomeComponent from './components/SomeComponent'
 
 ReactDOM.render(
   <SomeComponent/>,
